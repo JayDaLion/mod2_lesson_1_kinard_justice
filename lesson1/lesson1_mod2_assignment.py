0f149546130349f9cb09c503bda06a39751d6cb7
JayDaLion <Jaystuff123@gmail.com>
2024-09-13 11:27:59 -0700

    Kinard, Justice assignments Mod 2 Lesson1

---
+++ lesson1/lesson1_mod2_assignment.py
@@ -0,0 +1,34 @@
+#showcase your dance moves
+weather = "sunny"
+if weather == "sunny":
+    print("wear sunglasses!")
+else:
+    print ("Take an umbrella!")
+
+#Your mood today
+emotion = input ("How do you feel today Happy or Sad?: ")
+emotion2 = emotion
+if emotion == "happy":
+    print("That's great to hear!")
+elif emotion == "sad":
+    print ("I hope your day gets better!")    
+
+#python naming convention: Didnt get this one seems to be right 
+pi_value = 3.14
+user_age = 25
+user_location = "New York"
+MAXLIMIT = 1000
+
+#Arithmetic operations in daily life
+bread = 2.99
+peanut_butter = 3.99
+jelly = 4
+cart_total = bread + peanut_butter + jelly
+print(cart_total)
+
+total_account_balance = 10000
+interest_rate = 0.07
+#my math for the interest
+account_increase = interest_rate * total_account_balance
+yearend_account_increase = account_increase + total_account_balance
+print(yearend_account_increase)
